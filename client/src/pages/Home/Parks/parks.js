2b9fbf38f4834f360a030c54308bcf2ae8b3269d
blairyoung15 <bey99d@gmail.com>
2022-07-12 20:23:17 -0500

    activities container added

--- client/src/pages/Home/Parks/parks.js
+++ client/src/pages/Home/Parks/parks.js
@@ -36,23 +36,42 @@ const parks = [
 const Parks = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <Container>
-                <Typography sx={{ fontWeight: 500, m: 2, color: 'success.main' }} variant="h6" component="div">
-                    National Parks
-                </Typography>
-                <Typography sx={{ fontWeight: 600, m: 5 }} variant="h4" component="div">
-                    Select a National Park to view upcoming activities
-                </Typography>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {
-                        parks.map(park => <Park
-                            key={park.name}
-                            park={park}
-                        ></Park>)
-                    }
-                </Grid>
-            </Container>
-        </Box>
+        <Container>
+            <Typography sx={{ fontWeight: 500, m: 2, color: 'success.main' }} variant="h6" component="div">
+                National Parks
+            </Typography>
+            <Typography sx={{ fontWeight: 600, m: 5 }} variant="h4" component="div">
+                Select a National Park to view upcoming activities
+            </Typography>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {
+                    parks.map(park => <Park
+                        key={park.name}
+                        park={park}
+                    ></Park>)
+                }
+            </Grid>
+        </Container>
+
+
+        <Container>
+            <Typography sx={{ fontWeight: 500, m: 3, color: 'success.main' }} variant="h6" component="div">
+                Activities
+            </Typography>
+            <Typography sx={{ fontWeight: 600, m: 5 }} variant="h5" component="div">
+                Select from the activities below to add to your itinerary
+            </Typography>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {
+                    parks.map(activity => <Activity
+                        key={activity.title}
+                        park={activity}
+                    ></Activity>)
+                }
+            </Grid>
+        </Container>
+
+    </Box>
     );
 };
 
